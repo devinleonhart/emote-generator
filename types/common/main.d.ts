5521bf1093ad7e0db196ebceeeec47c05f9c9464
Devin Leonhart <devinleonhart@fastmail.com>
2022-05-15 18:18:54 -0700

    Remove unused types.

--- types/common/main.d.ts
+++ types/common/main.d.ts
@@ -1,11 +1,3 @@
-interface AvailableEmoteParts {
-  [key: string]: string[];
-  head: string[];
-  eyes: string[];
-  eyebrows: string[];
-  mouth: string[];
-}
-
 interface BlueprintCollection {
   [key: string]: Blueprint
 }
@@ -18,28 +10,8 @@ interface Blueprint {
   mouth: string;
 }
 
-interface EmoteParts {
-  [key: string]: string;
-  head: string;
-  eyes: string;
-  eyebrows: string;
-  mouth: string;
-}
-
 interface Part {
   key: string;
   name: string;
   src: Buffer;
-}
-
-interface QueryParams {
-  [key: string]: string;
-}
-
-interface SelectedParts {
-  [key: string]: string | null;
-  head: string | null;
-  eyes: string | null;
-  eyebrows: string | null;
-  mouth: string | null;
 }
